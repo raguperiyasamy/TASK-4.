3247a2c6a9f279243c092ca574905c0cd5c4b8b7
raguperiyasamy <111695592+raguperiyasamy@users.noreply.github.com>
2022-08-26 13:01:17 +0530

    Add files via upload

---
+++ obs.js
@@ -0,0 +1,31 @@
+{
+    "id": "1",
+    "details": [
+        {
+        "name": "kumarn",
+        "person": "1",
+        "age": 25
+      },
+      {
+        "name": "raja",
+        "person": "1",
+        "age": 10
+      }
+    ],
+  }
+
+  {
+    "id": "2",
+    "details": [
+        {
+        "name": "rajkumar",
+        "person": "1",
+        "age": 29
+      },
+      {
+        "name": "velraj",
+        "person": "1",
+        "age": 15
+      }
+    ],
+  }
